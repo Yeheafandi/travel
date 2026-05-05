31194c7a6985a43ec7dea079dcfd81f10269cb79
SaleemAlmahde <almahdesaleem@gmail.com>
2026-05-05 21:08:15 +0300

    migrate telegram bot from flutter to dart server

--- try_wejha_bot/lib/core/config/telegram_config.dart
+++
@@ -1,5 +0,0 @@
-class TelegramConfig {
-  static const String botToken =
-      '8647364911:AAFfO9MzR8r8pHX0VkCktMfE7lux9jadhtk';
-  static const String baseUrl = 'https://api.telegram.org/bot$botToken';
-}
